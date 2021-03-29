cd2714964c3fe4934dfbd69a8df364104337196b
Olatunji Omisakin <msknolatunji@gmail.com>
2021-03-29 12:30:03 +0100

    Typing Test Project

--- Ex_Files_JavaScript_EssT/JS/script.js
+++ Ex_Files_JavaScript_EssT/JS/script.js
@@ -1,27 +1,74 @@
-const HOURHAND = document.querySelector("#hour");
-const MINHAND = document.querySelector("#minute");
-const SECHAND = document.querySelector("#second");
+const testWrapper = document.querySelector(".test-wrapper");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p").innerHTML;
+const resetButton = document.querySelector("#reset");
+const theTimer = document.querySelector(".timer");
 
-var date = new Date();
-var hr = date.getHours();
-var min = date.getMinutes();
-var sec = date.getSeconds();
+// Add leading zero to numbers 9 or below (purely for aesthetics):
 
-let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
-let minPosition = (min * 360 / 60) + ( sec * (360 / 60) / 60);
-let secPosition = sec * 360 / 60;
+var timer = [0,0,0,0];
+var interval;
+var timeRunning = false;
 
-function runTheClock() {
-
-  hrPosition = hrPosition + (3/360)
-
-  minPosition = minPosition + (6 / 60)
-
-  secPosition = secPosition + 6;
-
-  HOURHAND.style.transform = 'rotate(' + hrPosition + 'deg)';
-  MINHAND.style.transform = 'rotate(' + minPosition + 'deg)';
-  SECHAND.style.transform = 'rotate(' + secPosition + 'deg)';
+function leadingZero(time) {
+  if(time <= 9) {
+    time = "0" + time;
+  }
+  return time;
 }
 
-var interval =setInterval(runTheClock, 1000);
+// Run a standard minute/second/hundredths timer:
+
+function runTimeout() {
+  const currentTimer = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+  theTimer.innerHTML = currentTimer;
+  timer[3]++;
+
+  timer[0] = Math.floor((timer[3] / 100) / 60);
+  timer[1] = Math.floor((timer[3] / 100) - (timer[0] * 60));
+  timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+}
+
+// Match the text entered with the provided text on the page:
+function spellCheck() {
+  let textEntered = testArea.value;
+  const originTextMatch = originText.substring(0, textEntered.length);
+  if(textEntered == originText) {
+    clearInterval(interval);
+    testWrapper.style.borderColor = "#429890"
+  } else {
+    if (textEntered == originTextMatch) {
+      testWrapper.style.borderColor = "#65CCF3";
+    } else {
+      testWrapper.style.borderColor = "#E95D0F"
+    }
+  }
+  console.log(textEntered); 
+}
+
+// Start the timer:
+function start() {
+  let textEnteredLength = testArea.value.length;
+  if(textEnteredLength === 0 && !timeRunning) {
+    timeRinning = true;
+    interval = setInterval(runTimeout, 10);
+  }
+  console.log(textEnteredLength)
+}
+
+// Reset everything:
+function reset() {
+  clearInterval(interval);
+  interval = null;
+  timer = [0,0,0,0];
+  timeRunning = false;
+
+  testArea.value = "";
+  theTimer.innerHTML = "00:00:00";
+  testWrapper.style.borderColor = "grey";
+}
+
+// Event listeners for keyboard input and the reset button:
+testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click", reset, false);
